442db0244f3d02ea05a1b9e635b734115115c58b
Nidurolak <jaewoon08@gmail.com>
2024-10-21 22:36:42 +0900

    feat : 메이커 관련 정보 업로드

--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -10,6 +10,7 @@ import title from '../assets/title.png';
 import aj from '../assets/aj.png';
 import navis from '../assets/navis.png';
 import 재우 from '../assets/재우.png';
+import 메이커 from '../assets/메이커.webp';
 
 
 function ProjectDisplay(value: ProjectTemplet) {
@@ -26,9 +27,9 @@ function ProjectDisplay(value: ProjectTemplet) {
     useLayoutEffect(() => {
         const interval = setInterval(() => {
             switch (ProjectExplainState) {
-                case 'Navis': setProjectExplainState("재우's AI다운로더")
+                case 'Navis': setProjectExplainState("재우's AI 다운로더")
                     break;
-                case "재우's AI 다운로더": setProjectExplainState('AJGAG')
+                case "재우's AI 다운로더": setProjectExplainState("재우's AI 메이커")
                     break;
                 case "재우's AI 메이커": setProjectExplainState('AJGAG')
                     break;
@@ -51,11 +52,23 @@ function ProjectDisplay(value: ProjectTemplet) {
                     {ExplainOn && <InfoDisplay>
                         <h3>2023.3 ~ 2023.4</h3>
                         <h4>소규모 팀 업무 및 일정, 과제 관리 서비스</h4>
-                        <h4>사용 기술 : React, Recoil, React-Query & Styled-Component, Axios</h4>
+                        <h5>사용 기술 : React, Recoil, React-Query & Styled-Component, Axios</h5>
                     </InfoDisplay>}
                 </DisPlayIMG>
             </ProjectBox>;
             break;
+        case "재우's AI 메이커":
+            componentToRender = <ProjectBox variants={ProjectVariant} initial='init' animate='visible' exit='exit' key='EXPKey1'>
+                <h2>{ProjectExplainState}</h2>
+                <DisPlayIMG image={메이커} onMouseEnter={() => setExplainOn(true)} onMouseLeave={() => setExplainOn(false)}>
+                    {ExplainOn && <InfoDisplay>
+                        <h3>2024.6 ~ 2024.8</h3>
+                        <h4>게임 마비노기의 AI 직접 생성 및 편집 기능</h4>
+                        <h5>사용 기술 : TypeScript, React, Recoil, Styled-Component, Framer-Motion</h5>
+                    </InfoDisplay>}
+                </DisPlayIMG>
+
+            </ProjectBox>; break;
         case "재우's AI 다운로더":
             componentToRender = <ProjectBox variants={ProjectVariant} initial='init' animate='visible' exit='exit' key='EXPKey1'>
                 <h2>{ProjectExplainState}</h2>
@@ -63,7 +76,7 @@ function ProjectDisplay(value: ProjectTemplet) {
                     {ExplainOn && <InfoDisplay>
                         <h3>2023.12 ~ 2024.1</h3>
                         <h4>게임 마비노기의 AI 정보 열람 및 다운로드 사이트</h4>
-                        <h4>사용 기술 : TypeScript, React, Recoil, Styled-Component, Framer-Motion</h4>
+                        <h5>사용 기술 : TypeScript, React, Recoil, Styled-Component, Framer-Motion</h5>
                     </InfoDisplay>}
                 </DisPlayIMG>
 
@@ -75,7 +88,7 @@ function ProjectDisplay(value: ProjectTemplet) {
                     {ExplainOn && <InfoDisplay>
                         <h3>2023.8</h3>
                         <h4>아재 개그를 올리고 평가를 주고받는 사이트</h4>
-                        <h4>사용 기술 : TypeScript, React, Recoil, React-Query & Styled-Component, Axios</h4>
+                        <h5>사용 기술 : TypeScript, React, Recoil, React-Query & Styled-Component, Axios</h5>
                     </InfoDisplay>}
                 </DisPlayIMG>
             </ProjectBox>; break;
@@ -157,5 +170,6 @@ background-color:  rgba(240, 240, 240, 0);
   h2 {word-spacing: 1px; word-break:keep-all; font-weight: 700; color: rgba(30, 30, 30, 0.9); font-size: 55px; font-family: inherit;}
   h3 {word-spacing: 1px; word-break:keep-all; font-weight: 400; color: gray; font-size: 25px; font-family: inherit;}
   h4 {word-spacing: 1px; word-break:keep-all; font-weight: 400; color: black; font-size: 17px; font-family: inherit;}
+  h5 {word-spacing: 1px; word-break:keep-all; font-weight: 400; color: gray; font-size: 13px; font-family: inherit;}
 
 `
