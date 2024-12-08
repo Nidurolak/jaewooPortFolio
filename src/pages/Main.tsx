5dea2025cab96ede4ad1c2de017e97503e2198f2
Nidurolak <jaewoon08@gmail.com>
2024-12-09 00:03:28 +0900

    feat : 애니메이션 수정 중

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -30,16 +30,18 @@ function Main() {
         setClickedIndex(clickedIndex === index ? null : index);
     };
     const positions = [
-        { top: "5%", left: "30%" },
-        { top: "5%", left: "50%" },
-        { top: "50%", left: "30%" },
-        { top: "50%", left: "50%" },
+        //좌표 찌그러지는 위치 조정
+        { top: "5%", left: "30%", clickedTop: "10%", clickedLeft: "10%" },
+        { top: "5%", left: "50%", clickedTop: "25%", clickedLeft: "10%" },
+        { top: "50%", left: "30%", clickedTop: "40%", clickedLeft: "10%" },
+        { top: "50%", left: "50%", clickedTop: "55%", clickedLeft: "10%" },
     ];
 
     return (
-        <Container onClick={() => handleClick(null)}>
+        <AnimatePresence mode='wait'>
+            <Container onClick={() => handleClick(null)}>
 
-            {/*
+                {/*
             <MainTitle />
             <AnimatePresence>
                 <Element name="AboutMe"><AboutMeList /></Element>
@@ -47,54 +49,93 @@ function Main() {
 
             </AnimatePresence>
             <Element name="Project"><ProjectList /></Element>
-            <Element name="Contact"><ContactList /></Element>*/}
-            {Array.from({ length: 4 }).map((_, index) => (
+            <Element name="Contact"><ContactList /></Element>
                 <Card
-                    key={index + "card"}
-                    index={index}
-                    onMouseEnter={() => handleMouseEnter(index)}
-                    onMouseLeave={handleMouseLeave}
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        handleClick(index);
-                    }}
-                    style={{
-                        top: positions[index].top,
-                        left: positions[index].left,
-                    }}
+                    key={0 + "card"} index={0}
+                    onMouseEnter={() => handleMouseEnter(0)} onMouseLeave={handleMouseLeave}
+                    onClick={(e) => { e.stopPropagation(); handleClick(0); }}
+                    style={{ top: positions[0].top, left: positions[0].left }}
                     animate={{
-                        top:
-                            clickedIndex === index
-                                ? "0%" // 클릭된 카드의 상단 기준점
-                                : clickedIndex !== null
-                                    ? "0%" // 클릭된 상태에서 다른 카드의 상단 기준점
-                                    : positions[index].top, // 기본 상단 기준점
-                        left:
-                            clickedIndex === index
-                                ? "0%" // 클릭된 카드의 상단 기준점
-                                : clickedIndex !== null
-                                    ? "0%" // 클릭된 상태에서 다른 카드의 상단 기준점
-                                    : positions[index].left, // 기본 상단 기준점
-                        width:
-                            clickedIndex === index
-                                ? "100vw" // 클릭된 카드의 가로 크기
-                                : clickedIndex !== null
-                                    ? "0px" // 클릭된 상태에서 다른 카드의 가로 크기
-                                    : "300px", // 기본 가로 크기
-                        height:
-                            clickedIndex === index
-                                ? "100vh" // 클릭된 카드의 세로 크기
-                                : clickedIndex !== null
-                                    ? "0px" // 클릭된 상태에서 다른 카드의 세로 크기
-                                    : "400px", // 기본 세로 크기
+                        top: clickedIndex === 0 ? "0%" // 클릭된 카드의 상단 기준점
+                            : clickedIndex !== null
+                                ? "0%" // 클릭된 상태에서 다른 카드의 상단 기준점
+                                : positions[0].top, // 기본 상단 기준점
+                        left: clickedIndex === 0
+                            ? "0%" // 클릭된 카드의 상단 기준점
+                            : clickedIndex !== null
+                                ? "0%" // 클릭된 상태에서 다른 카드의 상단 기준점
+                                : positions[0].left, // 기본 상단 기준점
+                        width: clickedIndex === 0
+                            ? "100vw" // 클릭된 카드의 가로 크기
+                            : clickedIndex !== null
+                                ? "0px" // 클릭된 상태에서 다른 카드의 가로 크기
+                                : "300px", // 기본 가로 크기
+                        height: clickedIndex === 0
+                            ? "100vh" // 클릭된 카드의 세로 크기
+                            : clickedIndex !== null
+                                ? "0px" // 클릭된 상태에서 다른 카드의 세로 크기
+                                : "400px", // 기본 세로 크기
                     }}
                     transition={{ duration: 0.7 }}
-                />
-            ))}
+                />*/}
 
-        </Container>)
+                {Array.from({ length: 4 }).map((_, index) => (
+
+                    <Card
+                        key={index + "card"}
+                        index={index}
+                        selected={clickedIndex}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleClick(index);
+                        }}
+                        style={{
+                            top: positions[index].top,
+                            left: positions[index].left,
+                        }}
+                        animate={{
+                            top:
+                                clickedIndex === index
+                                    ? "10%" // 클릭된 카드의 상단 기준점
+                                    : clickedIndex !== null
+                                        ? positions[index].clickedTop // 클릭된 상태에서 다른 카드의 상단 기준점
+                                        : positions[index].top, // 기본 상단 기준점
+                            left:
+                                clickedIndex === index
+                                    ? "15%" // 클릭된 카드의 상단 기준점
+                                    : clickedIndex !== null
+                                        ? "1%" // 클릭된 상태에서 다른 카드의 상단 기준점
+                                        : positions[index].left, // 기본 상단 기준점
+                            width:
+                                clickedIndex === index
+                                    ? "70vw" // 클릭된 카드의 가로 크기
+                                    : clickedIndex !== null
+                                        ? "150px" // 클릭된 상태에서 다른 카드의 가로 크기
+                                        : "300px", // 기본 가로 크기
+                            height:
+                                clickedIndex === index
+                                    ? "80vh" // 클릭된 카드의 세로 크기
+                                    : clickedIndex !== null
+                                        ? "100px" // 클릭된 상태에서 다른 카드의 세로 크기
+                                        : "400px", // 기본 세로 크기
+                            /*opacity:
+                                clickedIndex === index
+                                    ? 1 // 클릭된 카드의 세로 크기
+                                    : clickedIndex !== null
+                                        ? 0 // 클릭된 상태에서 다른 카드의 세로 크기
+                                        : 1, // 기본 세로 크기*/
+                        }}
+                        transition={{ ease: "easeInOut", duration: clickedIndex === index ? 1.7 : clickedIndex !== null ? 1.3 : 0.8 }}
+
+                    ><h2>테스트</h2></Card>
+                ))}
+            </Container></AnimatePresence>)
 }
+/*
 
+ */
 export default Main;
 
 const CardBox = styled.div`
@@ -119,18 +160,20 @@ justify-content: center;
   }
 `
 
-const Card = styled(motion.div) <{ index: number }>`
+const Card = styled(motion.div) <{ index: number, selected: number | null }>`
 position: absolute;
     width: 300px;
     height: 400px;
     display: flex;
+    background-color: rgba(30,30,30,1);
     border:  9px solid rgba(255, 255, 255, 0.8);
     border-radius: 10px;
 align-items: center;
-  transition: transform 0.3s;
+z-index: ${({ index, selected }) => (index === selected ? 999 : index)};
 &:hover {
     cursor: pointer;
 }
+
 `
 
 const Container = styled.div`
